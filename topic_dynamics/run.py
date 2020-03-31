783d0f3f08bc26ab3ff39efc2fd1c04199419786
areyde <areyde.elvgren@gmail.com>
2020-03-31 17:54:27 +0300

    Added the saving of the parameters

--- topic_dynamics/run.py
+++ topic_dynamics/run.py
@@ -2,6 +2,8 @@
 Script for running the entire pipeline from the command line
 """
 import argparse
+import datetime
+import os
 
 from .language_recognition.utils import main as initialize_enry
 from .modeling import model_topics
@@ -14,6 +16,41 @@ def main(args: argparse.Namespace) -> None:
     :param args: arguments of parsing and modeling.
     :return: None.
     """
+    if not os.path.exists(args.output):
+        os.makedirs(args.output)
+    with open(os.path.abspath(os.path.join(args.output, "log.txt")), "w+") as fout:
+        fout.write("Topic modeling pipeline launched: {datetime}\n"
+                   "\n"
+                   "--Slicing parameters--\n"
+                   "Starting date: {start_date}\n"
+                   "Number of temporal slices: {slices}\n"
+                   "Difference between slices in days: {days}\n"
+                   "\n"
+                   "--Parsing parameters--\n"
+                   "A list of input projects: {input}\n"
+                   "An output folder: {output}\n"
+                   "Parsing mode: {mode}\n"
+                   "\n"
+                   "--Modeling parameters--\n"
+                   "Number of topics: {topics}\n"
+                   "Sparse Theta parameter: {sparse_theta}\n"
+                   "Sparse Phi parameter: {sparse_phi}\n"
+                   "Decorrelator Phi parameter: {decorrelator_phi}\n"
+                   "Number of document passes: {document_passes}\n"
+                   "Number of collection passes: {collection_passes}\n"
+                   "\n"
+                   "--Statistics parameters--\n"
+                   "Number of topical files to save: {topical_files}"
+                   .format(datetime=datetime.datetime.now().strftime("%Y-%m-%d %H:%M"),
+                           start_date=args.start_date, slices=args.slices, days=args.days,
+                           input=args.input, output=args.output, mode=args.mode,
+                           topics=args.topics, sparse_theta=args.sparse_theta,
+                           sparse_phi=args.sparse_phi, decorrelator_phi=args.decorrelator_phi,
+                           document_passes=args.document_passes,
+                           collection_passes=args.collection_passes,
+                           topical_files=args.topical_files))
+    with open(os.path.abspath(os.path.join(args.output, "log.txt"))) as fin:
+        print(fin.read())
     initialize_parser()
     initialize_enry()
     if args.mode == "diffs":
