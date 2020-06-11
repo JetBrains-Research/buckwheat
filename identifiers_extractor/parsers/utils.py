c9058332511fc79c60a20e51112bf0dce4c6e61d
areyde <areyde.elvgren@gmail.com>
2020-06-11 19:32:46 +0300

    Test the binary release of tree-sitter on GitHub Actions

--- identifiers_extractor/parsers/utils.py
+++ identifiers_extractor/parsers/utils.py
@@ -2,6 +2,7 @@
 Tree-sitter related functionality.
 """
 import os
+import urllib.request
 
 from tree_sitter import Language, Parser
 
@@ -31,46 +32,12 @@ def main() -> None:
     Initialize tree-sitter library.
     :return: None.
     """
-    # root directory for tree-sitter
-    tree_sitter_dir = get_tree_sitter_dir()
-    # grammar locations
-    javascript_grammar_loc = os.path.join(tree_sitter_dir, "vendor", "tree-sitter-javascript")
-    java_grammar_loc = os.path.join(tree_sitter_dir, "vendor", "tree-sitter-java")
-    python_grammar_loc = os.path.join(tree_sitter_dir, "vendor", "tree-sitter-python")
-    go_grammar_loc = os.path.join(tree_sitter_dir, "vendor", "tree-sitter-go")
-    cpp_grammar_loc = os.path.join(tree_sitter_dir, "vendor", "tree-sitter-cpp")
-    ruby_grammar_loc = os.path.join(tree_sitter_dir, "vendor", "tree-sitter-ruby")
-    typescript_grammar_loc = os.path.join(tree_sitter_dir,
-                                          "vendor", "tree-sitter-typescript", "typescript")
-    tsx_grammar_loc = os.path.join(tree_sitter_dir, "vendor", "tree-sitter-typescript", "tsx")
-    php_grammar_loc = os.path.join(tree_sitter_dir, "vendor", "tree-sitter-php")
-    c_sharp_grammar_loc = os.path.join(tree_sitter_dir, "vendor", "tree-sitter-c-sharp")
-    c_grammar_loc = os.path.join(tree_sitter_dir, "vendor", "tree-sitter-c")
-    bash_grammar_loc = os.path.join(tree_sitter_dir, "vendor", "tree-sitter-bash")
-    rust_grammar_loc = os.path.join(tree_sitter_dir, "vendor", "tree-sitter-rust")
-    # location for library
-    bin_loc = get_tree_sitter_so()
-    # build everything
-    Language.build_library(
-        # Store the library in the `bin_loc`
-        bin_loc,
-        # Include languages
-        [
-            javascript_grammar_loc,
-            python_grammar_loc,
-            java_grammar_loc,
-            go_grammar_loc,
-            cpp_grammar_loc,
-            ruby_grammar_loc,
-            typescript_grammar_loc,
-            tsx_grammar_loc,
-            php_grammar_loc,
-            c_sharp_grammar_loc,
-            c_grammar_loc,
-            bash_grammar_loc,
-            rust_grammar_loc
-        ]
-    )
+    url = "https://github.com/areyde/buckwheat/releases/download/v1.1.1/langs.so"
+    filename = "langs.so"
+    if not os.path.exists(get_tree_sitter_so()):
+        urllib.request.urlretrieve(url,
+                                   os.path.abspath(os.path.join(get_tree_sitter_dir(), "build",
+                                                                filename)))
     print("Parser successfully initialized.")
 
 
