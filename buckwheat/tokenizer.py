bd5554c7bd3b0a243d17f6d22dd7817e1912e2ce
tikerlade <tikerlade@gmail.com>
2020-07-15 12:08:56 +0300

    Fixed typo in buckwheat/tokenizer.py
    Co-authored-by: Egor Bulychev <egorbu@gmail.com>

--- buckwheat/tokenizer.py
+++ buckwheat/tokenizer.py
@@ -269,7 +269,7 @@ class TreeSitterParser:
         """
         Given a file and language of this file. Extract comments from this file.
         :param file: the path to file.
-        :param lang: the language of code.
+        :param lang: programming language.
         :return: list of comments.
         """
         comments = []
